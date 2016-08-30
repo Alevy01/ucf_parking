6fc7927519eebf59fe31a879dcc990468558f4eb
Adam Levy <Alevy01@gmail.com>
2016-08-29 23:36:43 -0400

    return body

--- send.py
+++ send.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming text with a simple text message."""
-    body = request.form.getlist('Body')
-    resp = twilio.twiml.Response()
-    resp.message("This is your body. " + str(body[0]))
-    return str(resp)
+  """Respond to incoming text with a simple text message."""
+  body = request.form.getlist('Body')
+  resp = twilio.twiml.Response()
+  resp.message("This is your body. " + str(body[0]))
+  return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
