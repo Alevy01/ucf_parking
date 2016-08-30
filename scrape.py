cf91e14ca32b75efad2daeda55258036a6d6ccad
Adam Levy <Alevy01@gmail.com>
2016-08-29 23:36:10 -0400

    formatted for lambda

--- scrape.py
+++ scrape.py
@@ -1,20 +1,25 @@
 import bs4
 import urllib2
+import json
 import BeautifulSoup
 
-r = urllib2.urlopen('http://secure.parking.ucf.edu/GarageCount/').read()
-bs = BeautifulSoup.BeautifulSoup(r)
-
-garages = bs.findAll(lambda tag: tag.name=='tr' and tag.has_key('id') and "ctl00_MainContent_gvCounts_DXDataRow" in tag["id"])
-
-for g in garages:
-  name = g.find(lambda tag: tag.name=='td' and tag.has_key('class') and tag["class"] == 'dxgv').getText().split(' ')[1]
-  try:
-    available = g.find(lambda tag: tag.name=='td' and tag.has_key('id') and "_2" in tag['id']).getText().split('/')[0]
-    print name + " " + available
-  except AttributeError:
-    print name
+def lambda_handler():
+  r = urllib2.urlopen('http://secure.parking.ucf.edu/GarageCount/').read()
+  bs = BeautifulSoup.BeautifulSoup(r)
   
+  garages = bs.findAll(lambda tag: tag.name=='tr' and tag.has_key('id') and "ctl00_MainContent_gvCounts_DXDataRow" in tag["id"])
   
+  data = {}
+  
+  for g in garages:
+    name = g.find(lambda tag: tag.name=='td' and tag.has_key('class') and tag["class"] == 'dxgv').getText().split(' ')[1]
+    try:
+      available = g.find(lambda tag: tag.name=='td' and tag.has_key('id') and "_2" in tag['id']).getText().split('/')[0]
+      data[name] = available
+    except AttributeError as e:
+      print e
+      
+  print json.dumps(data)
 
 
+lambda_handler()
