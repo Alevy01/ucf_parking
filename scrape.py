9802c9fef2dd0910b045b0c78a5576769e1ace9a
Adam Levy <Alevy01@gmail.com>
2016-09-01 10:31:21 -0400

    logic built to find users who need to be texted. Just need garage info now

--- scrape.py
+++ scrape.py
@@ -2,6 +2,12 @@ import bs4
 import urllib2
 import json
 import BeautifulSoup
+import psycopg2
+import datetime
+import twilio.twiml
+from twilio.rest import TwilioRestClient
+import config
+
 
 def lambda_handler(event, context):
   r = urllib2.urlopen('http://secure.parking.ucf.edu/GarageCount/').read()
@@ -19,4 +25,37 @@ def lambda_handler(event, context):
     except AttributeError as e:
       print e
       
-  return json.dumps(data)
+
+  g_data = json.dumps(data)
+  conn = psycopg2.connect(dbname="parking_test_db", user="alevy", password="parkingdb", host="parking-test-db.cwhygsajux1b.us-east-1.rds.amazonaws.com", port=5432)
+  cur = conn.cursor()
+  today = datetime.datetime.today().weekday()
+  time = datetime.datetime.now().time()
+  hour = time.hour
+  minute = time.minute
+  if minute >=53:
+    hour = hour + 1
+  
+  minute = myround(minute)
+  now = str(hour) + str(minute)
+  if len(now) != 4:
+    now += '0'
+
+  query = 'SELECT * from "user_texts" where "day"='+str(today)+' AND "time"='+str(now)+';'
+  cur.execute(query)
+  users = cur.fetchall();
+
+  account = config.twilio_config['account']
+  token = config.twilio_config['token']
+  client = TwilioRestClient(account, token)
+
+  for user in users:
+    num = user[0]
+    message = client.messages.create(to=num, from_=config.twilio_config['number'], body="")
+  
+def myround(x, base=15):
+  return int(base * round(float(x)/base)) % 60
+
+event = {'building' : 'A'}
+lambda_handler(event, '')
+
