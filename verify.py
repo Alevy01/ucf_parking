6711ab22eb35c203d0cd1a9737fe4097c2a4d4a1
Adam Levy <Alevy01@gmail.com>
2016-09-05 15:16:25 -0400

    verification lambda function

---
+++ verify.py
@@ -0,0 +1,15 @@
+from random import randint
+import twilio.twiml
+from twilio.rest import TwilioRestClient
+import config
+
+
+
+def lambda_hanlder(event,context):
+  verification_code = randint(10000,99999)
+  phone_number = event['phone_number']
+  account = config.twilio_config['account']
+  token = config.twilio_config['token']
+  client = TwilioRestClient(account,token)
+  client.messages.create(to=phone_number,from_=config.twilio_config["number"],body='Your verification code is '+ str(verification_code))
+  return verification_code
